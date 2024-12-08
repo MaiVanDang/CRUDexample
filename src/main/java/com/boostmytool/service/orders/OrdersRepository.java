59f9eb0b48f3f22740825819dd99638ca03f0b41
MaiVanDang <maivandang24@gmail.com>
2024-12-08 15:09:36 +0700

    oop

--- src/main/java/com/boostmytool/service/orders/OrdersRepository.java
+++
@@ -1,6 +0,0 @@
-package com.boostmytool.service.orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.boostmytool.model.orders.Order;
-public interface OrdersRepository extends JpaRepository<Order, String>{
-	
-}
--- src/main/java/com/boostmytool/services/customers/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boostmytool.services.customers;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.boostmytool.model.customers.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, String>{
-
-}
